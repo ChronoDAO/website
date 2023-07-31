827ba4dae9a5433167a90ccc8abbd21283015767
KarineDHoshi <karinedhoshi@yahoo.com>
2023-07-31 17:18:38 +0200

    KD Create DAO Sections

--- src/pages/AboutDao/AboutDao.tsx
+++ src/pages/AboutDao/AboutDao.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function AboutDao() {
   return (
-    <main>AboutDao</main>
+    <main>AboutDao
+
+      <section id='treso' alt=''>
+    
+      </section>
+
+      <section id='presDAO' alt=''>
+
+      </section>
+
+      <section id='roadMap' alt=''>
+
+      </section>
+
+    </main>
   )
 }
