b009158dddabe6d15f5b47d37bf79053de1e8158
KarineDHoshi <karinedhoshi@yahoo.com>
2023-07-31 21:01:07 +0200

    KD Add grid to DAO page

--- src/pages/AboutDao/AboutDao.tsx
+++ src/pages/AboutDao/AboutDao.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
-
+import './AboutDao.scss' 
 export default function AboutDao() {
   return (
-    <main>AboutDao
+    <main className='container'>
+      <section className='row-1' id='presDAO' alt=''>
+        <h2>La DAO</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni, doloribus, error cupiditate mollitia repellat, porro doloremque beatae delectus assumenda quo officiis sed nobis impedit facere maxime sequi dolor perferendis.</p>
+        <br/>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ratione magnam, quaerat accusantium qui cumque expedita laborum excepturi? Delectus ipsa, ratione temporibus quod neque officiis vero nulla veniam eaque modi!</p> 
+        <br/>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ratione magnam, quaerat accusantium qui cumque expedita laborum excepturi? Delectus ipsa, ratione temporibus quod neque officiis vero nulla veniam eaque modi!</p> 
+        <br/>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ratione magnam, quaerat accusantium qui cumque expedita laborum excepturi? Delectus ipsa, ratione temporibus quod neque officiis vero nulla veniam eaque modi!</p> 
 
-      <section id='treso' alt=''>
-    
+       </section>
+
+      <section className="row-1" id='treso' alt=''>
+        <h2>Notre trésorerie</h2>
+        <img src="http://placehold.it/300x250" id="capital" alt="" />
       </section>
 
-      <section id='presDAO' alt=''>
 
-      </section>
-
-      <section id='roadMap' alt=''>
-
+      <section className=' row-3' id='roadMap' alt=''>
+        <h2>Notre Roadmap:</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, excepturi. Inventore quo illo modi libero velit nisi dicta sed, et, totam est, quaerat eos iusto voluptatem explicabo laboriosam cupiditate nobis.</p>
       </section>
 
     </main>
