706213f5140dbeb719a5547f291d465043c5c4bc
KarineDHoshi <karinedhoshi@yahoo.com>
2023-08-01 22:12:37 +0200

    KD Turned grid into flexbox

--- src/pages/AboutDao/AboutDao.tsx
+++ src/pages/AboutDao/AboutDao.tsx
@@ -1,31 +1,53 @@
-import React from 'react'
-import './AboutDao.scss' 
+import React from 'react';
+import './AboutDao.scss';
+
 export default function AboutDao() {
   return (
-    <main className='container'>
-      <section className='row-1' id='presDAO' alt=''>
-        <h2>La DAO</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni, doloribus, error cupiditate mollitia repellat, porro doloremque beatae delectus assumenda quo officiis sed nobis impedit facere maxime sequi dolor perferendis.</p>
-        <br/>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ratione magnam, quaerat accusantium qui cumque expedita laborum excepturi? Delectus ipsa, ratione temporibus quod neque officiis vero nulla veniam eaque modi!</p> 
-        <br/>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ratione magnam, quaerat accusantium qui cumque expedita laborum excepturi? Delectus ipsa, ratione temporibus quod neque officiis vero nulla veniam eaque modi!</p> 
-        <br/>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ratione magnam, quaerat accusantium qui cumque expedita laborum excepturi? Delectus ipsa, ratione temporibus quod neque officiis vero nulla veniam eaque modi!</p> 
-
-       </section>
-
-      <section className="row-1" id='treso' alt=''>
-        <h2>Notre trésorerie</h2>
-        <img src="http://placehold.it/300x250" id="capital" alt="" />
-      </section>
-
-
-      <section className=' row-3' id='roadMap' alt=''>
-        <h2>Notre Roadmap:</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, excepturi. Inventore quo illo modi libero velit nisi dicta sed, et, totam est, quaerat eos iusto voluptatem explicabo laboriosam cupiditate nobis.</p>
-      </section>
-
-    </main>
-  )
+    <div className='main'>
+      <h1>A propos de ChronoDAO</h1>
+      <br/>
+      <div className="containerDAO">
+        <div className="content">
+          <h1>L'histoire de notre DAO</h1>
+          <br/>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nihil eaque porro quas exercitationem, tempore distinctio ipsam vero aperiam, enim quos inventore nulla odio repudiandae cum molestias fugiat laborum eveniet.
+            Vel ipsa vero velit molestiae est quisquam! Perspiciatis ullam perferendis aperiam inventore nam in provident cum magnam maiores, tenetur odio. Quia, enim quidem corrupti magnam delectus soluta maiores error accusamus?
+            Consequuntur id nisi possimus similique quas impedit sapiente illum ducimus, inventore incidunt enim, obcaecati itaque repellat natus nam quidem ad vel nihil, eaque iusto dolorem dicta. Odit officia ab deleniti!
+            Ullam ex, ab a illo eligendi iure pariatur sint quisquam, saepe dolorem consequuntur aliquid voluptatem, quam aperiam. Qui a nulla repudiandae ipsum iure harum, vero, odit in minus odio unde?
+            Libero unde nihil incidunt aut beatae odio facilis molestiae mollitia cumque officiis, delectus cum quia modi obcaecati, provident, nam cupiditate at laudantium commodi facere repudiandae blanditiis quos? Quo, delectus quae?</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nihil eaque porro quas exercitationem, tempore distinctio ipsam vero aperiam, enim quos inventore nulla odio repudiandae cum molestias fugiat laborum eveniet.
+            Vel ipsa vero velit molestiae est quisquam! Perspiciatis ullam perferendis aperiam inventore nam in provident cum magnam maiores, tenetur odio. Quia, enim quidem corrupti magnam delectus soluta maiores error accusamus?
+            Consequuntur id nisi possimus similique quas impedit sapiente illum ducimus, inventore incidunt enim, obcaecati itaque repellat natus nam quidem ad vel nihil, eaque iusto dolorem dicta. Odit officia ab deleniti!
+            Ullam ex, ab a illo eligendi iure pariatur sint quisquam, saepe dolorem consequuntur aliquid voluptatem, quam aperiam. Qui a nulla repudiandae ipsum iure harum, vero, odit in minus odio unde?
+            Libero unde nihil incidunt aut beatae odio facilis molestiae mollitia cumque officiis, delectus cum quia modi obcaecati, provident, nam cupiditate at laudantium commodi facere repudiandae blanditiis quos? Quo, delectus quae?</p>
+        </div>
+        <div className="sidebar">
+          <h1>Trésorerie</h1>
+          <img src='https://placehold.co/600x400' alt='Trésorerie'></img>
+        </div>
+      </div>
+      
+      <div className='containerMap'>
+        <div className="content2">
+          <h1>Road Map</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At iusto numquam libero ex quidem minus nesciunt fuga similique, voluptas officiis culpa laboriosam autem nulla perferendis incidunt reprehenderit ipsum eveniet. Excepturi.
+          Quia, enim! Quam itaque odit repellendus cum adipisci est earum, odio qui animi repellat aliquam laboriosam. Voluptatem, fugiat, ipsa aspernatur id earum asperiores quibusdam explicabo, tempora dignissimos ut odio ratione?
+          Ipsa, cum sapiente. Eum impedit quod fugiat culpa facilis dignissimos dolorem nobis fuga nesciunt beatae, vitae non aliquid aliquam corrupti ab temporibus soluta maxime at? Nisi, vero eos. Maxime, delectus.
+          Ex adipisci nemo necessitatibus quasi iusto corrupti illum. Perspiciatis sapiente laborum aliquid, doloremque ex repellendus eius, autem est unde, incidunt illum ab officiis explicabo corrupti non ipsam. Reiciendis, ad optio.
+          Iusto aut nam quam ab eum aliquid! Nesciunt ratione, in ducimus cum nisi unde non facilis molestias, odit, maiores ipsa? Reiciendis mollitia quos beatae eligendi illum iure consequuntur hic molestiae.
+          Ullam unde quod asperiores incidunt esse doloremque inventore recusandae cupiditate autem architecto? Velit ab ducimus debitis animi ex eos quidem at quo rem quibusdam. Similique cum porro numquam saepe minus!
+          Sunt ab accusamus voluptas, facilis dolore delectus harum. Rerum non molestias provident fuga nulla sint illo libero et, veniam voluptatibus, enim quam aliquid debitis minima, harum amet temporibus ratione atque.
+          Laborum aspernatur et architecto nihil quibusdam perspiciatis est officia perferendis enim nesciunt reprehenderit, sunt iste magnam officiis corporis temporibus alias at explicabo repellat deleniti ab dolores nam natus sequi? Placeat?
+          Laboriosam aut totam impedit autem quam illo sit pariatur sequi, a asperiores earum consequatur accusantium corrupti ratione reiciendis ullam obcaecati sint. Delectus, soluta adipisci laborum maiores dolore eos tempore commodi?
+          In ratione quas accusantium voluptatibus nemo dolorum, nostrum reprehenderit error, repellendus dolorem mollitia illo nihil ipsum quis consequuntur vel quod eum deleniti rem sapiente? Nostrum modi exercitationem unde odit ipsam!
+          Distinctio, neque beatae consectetur dolorem veniam in magni et, maiores totam minus sapiente perspiciatis fugit enim sit ut adipisci molestias assumenda! Delectus aliquid quidem eum nostrum ad explicabo. Ipsa, placeat!
+          Unde sapiente qui temporibus iste eaque obcaecati aspernatur accusantium expedita alias molestias pariatur neque quibusdam voluptatum, dicta magni sed rem. Libero vero odio accusantium, placeat obcaecati debitis enim ea tempora?
+          Architecto perferendis debitis est sit voluptatem repudiandae culpa dignissimos earum, quisquam rerum praesentium consequuntur in dolores, nesciunt laborum enim! Tenetur commodi voluptates, id repellendus consequuntur omnis magnam nisi itaque necessitatibus!
+          Quia, illum voluptate velit inventore alias assumenda ipsum repellat mollitia dicta corporis commodi vel nemo voluptas praesentium aspernatur nostrum adipisci earum quasi enim temporibus tempora sapiente. Reiciendis vel illo voluptatem?
+          Voluptatum at suscipit itaque incidunt fuga illum quaerat nisi, voluptate nobis praesentium quidem accusantium commodi sunt ratione, dignissimos architecto cum, quam sed aliquid earum vel explicabo minima sapiente! Obcaecati, odit!</p>
+        </div>
+      </div>
+    </div>
+  );
 }
