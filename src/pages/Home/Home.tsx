1d50b7209038f7a2350f55b4cbac2c2afe1b46ec
OlivierMaria <olivier-maria@hotmail.com>
2023-08-01 12:13:13 +0200

    OM position footer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,8 @@ import "./Home.scss"
 
 export default function Home() {
   return (
-    <main className="container">
-     <section className="row-1">
+    <main>
+     <section className="home__wrapper">
       <h2>DAO</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis quam illo, suscipit culpa repellendus quia voluptate animi dolorem sapiente, voluptates delectus! Nihil unde voluptate earum repudiandae aspernatur vitae magni culpa!
       Nam eveniet est, sed unde obcaecati similique magnam ab minus illo at laudantium quidem corrupti a perspiciatis exercitationem. Suscipit, ab doloribus? Aliquam necessitatibus magnam, impedit placeat ipsam deserunt tempora fugit!</p>
