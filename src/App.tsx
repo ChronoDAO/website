7af9fa8445c31a2c5351801b910050852dfd341b
OlivierMaria <olivier-maria@hotmail.com>
2023-08-01 11:39:29 +0200

    OM footer position

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import "./App.scss";
 import Nav from "./components/nav/Nav";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home/Home";
 import AboutUs from "./pages/AboutUs/AboutUs";
 import AboutDao from "./pages/AboutDao/AboutDao";
+import Footer from "./components/footer/Footer.tsx";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about_us" element={<AboutUs />} />
         <Route path="/about_dao" element={<AboutDao />} />
       </Routes>
-    </>
+      <Footer/>
+    </div>
   );
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import "./Home.scss"
 
 export default function Home() {
   return (
-    <main>
-      Bonjour
+    <main className="container">
+     <section className="row-1">
+      <h2>DAO</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis quam illo, suscipit culpa repellendus quia voluptate animi dolorem sapiente, voluptates delectus! Nihil unde voluptate earum repudiandae aspernatur vitae magni culpa!
+      Nam eveniet est, sed unde obcaecati similique magnam ab minus illo at laudantium quidem corrupti a perspiciatis exercitationem. Suscipit, ab doloribus? Aliquam necessitatibus magnam, impedit placeat ipsam deserunt tempora fugit!</p>
+     </section>
+     <section className="row-2">
+     <h2>DAO</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis quam illo, suscipit culpa repellendus quia voluptate animi dolorem sapiente, voluptates delectus! Nihil unde voluptate earum repudiandae aspernatur vitae magni culpa!
+      Nam eveniet est, sed unde obcaecati similique magnam ab minus illo at laudantium quidem corrupti a perspiciatis exercitationem. Suscipit, ab doloribus? Aliquam necessitatibus magnam, impedit placeat ipsam deserunt tempora fugit!</p>
+     </section>
+     <section className="row-3">
+     <h2>DAO</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis quam illo, suscipit culpa repellendus quia voluptate animi dolorem sapiente, voluptates delectus! Nihil unde voluptate earum repudiandae aspernatur vitae magni culpa!
+      Nam eveniet est, sed unde obcaecati similique magnam ab minus illo at laudantium quidem corrupti a perspiciatis exercitationem. Suscipit, ab doloribus? Aliquam necessitatibus magnam, impedit placeat ipsam deserunt tempora fugit!</p>
+     </section>
     </main>
   );
 }
