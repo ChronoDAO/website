cee887de7cee24e8eb0feda806f6641309030b07
Thomas <thomas.kanthack@gmail.com>
2023-08-01 12:08:26 +0200

    fix herobanner

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,8 +5,23 @@ import "./Home.scss";
 export default function Home() {
   return (
     <>
-      <main>
         <Banner />
+      <main>
+        <section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero numquam veniam laudantium adipisci placeat enim mollitia recusandae, quae ratione soluta eum quis eius laboriosam qui delectus optio laborum veritatis corporis?
+        </section>
+        <section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero numquam veniam laudantium adipisci placeat enim mollitia recusandae, quae ratione soluta eum quis eius laboriosam qui delectus optio laborum veritatis corporis?
+        </section>
+        <section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero numquam veniam laudantium adipisci placeat enim mollitia recusandae, quae ratione soluta eum quis eius laboriosam qui delectus optio laborum veritatis corporis?
+        </section>
+        <section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero numquam veniam laudantium adipisci placeat enim mollitia recusandae, quae ratione soluta eum quis eius laboriosam qui delectus optio laborum veritatis corporis?
+        </section>
+        <section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero numquam veniam laudantium adipisci placeat enim mollitia recusandae, quae ratione soluta eum quis eius laboriosam qui delectus optio laborum veritatis corporis?
+        </section>
       </main>
     </>
   );
