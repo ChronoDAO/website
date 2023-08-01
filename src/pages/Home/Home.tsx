dccb6e84ead5b9cec528e4529b2461e721f2cd4d
Thomas <thomas.kanthack@gmail.com>
2023-08-01 12:00:51 +0200

    adding hero on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Banner from "../../components/Banner/Banner";
+import "./Home.scss";
 
 export default function Home() {
   return (
-    <main>
-      Bonjour
-    </main>
+    <>
+      <main>
+        <Banner />
+      </main>
+    </>
   );
 }
