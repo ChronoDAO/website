0e39b4c9abb8cd10a7a17fa679793973a17997fb
Thomas <thomas.kanthack@gmail.com>
2023-08-02 14:00:05 +0200

    try to resolve eslint, tsx for vercel

--- src/components/AnimateGrid/AnimateGrid.tsx
+++ src/components/AnimateGrid/AnimateGrid.tsx
@@ -2,51 +2,57 @@ import { useEffect, useState } from "react";
 import "./AnimateGrid.scss";
 
 export default function AnimateGrid() {
-  //@ts-ignore
-  const [x, setX] = useState();
-  //@ts-ignore
-  const [y, setY] = useState();
+  const [, setX] = useState<number | undefined>();
+  const [, setY] = useState<number | undefined>();
 
   const data = import.meta.glob("/public/images/assets/mecha/*");
   const images: string[] = [];
   Object.entries(data).forEach(([key]) => images.push(key));
 
   useEffect(() => {
-    const update = (e) => {
-      setX(e.x);
-      setY(e.y);
+    const update = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      setX(e.clientX);
+      setY(e.clientY);
     };
-    //@ts-ignore
-    document.querySelector("section").addEventListener("mousemove", (e) => {
-      update(e);
-      const axeX = (e.x / window.innerWidth - 0.5) * 10;
-      const axeY = (e.y / window.innerHeight - 0.5) * 2;
-      let i = 1;
-      document.querySelectorAll(".images-grid > img").forEach((image) => {
-        i++;
-        image &&
-          //@ts-ignore
-          (image.style.transform = `translate(${(axeX *( i * 0.5)) / 0.5}px, ${
-            (axeY * (i * 0.5)) / 0.5
-          }px )`);
-      });
-    });
-    return () => {
-      window.removeEventListener("mousemove", update);
-      const homeSection = document.querySelector(".home-section__wrapper");
 
-      homeSection &&
-        homeSection.addEventListener("mouseleave", (e) => {
-          setTimeout(() => {
-            document
-              .querySelectorAll(".images-grid > img")
-              .forEach((element) => {
-                //@ts-ignore
-                element && (element.style.transform = `translate(0,0)`);
-              });
-          }, 500);
+    const sectionElement = document.querySelector("section") as HTMLDivElement;
+    if (sectionElement) {
+      sectionElement.addEventListener("mousemove", (e) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        update(e);
+        const axeX = (e.clientX / window.innerWidth - 0.5) * 10;
+        const axeY = (e.clientY / window.innerHeight - 0.5) * 2;
+        let i = 1;
+        document.querySelectorAll(".images-grid > img").forEach((image) => {
+          const imgElement = image as HTMLImageElement;
+          if (imgElement) {
+            imgElement.style.transform = `translate(${
+              (axeX * (i * 0.5)) / 0.5
+            }px, ${(axeY * (i * 0.5)) / 0.5}px )`;
+          }
+          i++;
         });
-    };
+      });
+
+      return () => {
+        const homeSection = document.querySelector(".home-section__wrapper");
+
+        homeSection &&
+          homeSection.addEventListener("mouseleave", () => {
+            setTimeout(() => {
+              document
+                .querySelectorAll(".images-grid > img")
+                .forEach((image) => {
+                  const imgElement = image as HTMLImageElement;
+                  if (imgElement) {
+                    imgElement.style.transform = `translate(0,0)`;
+                  }
+                });
+            }, 500);
+          });
+      };
+    }
   }, [setX, setY]);
 
   return (
