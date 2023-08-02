f83641663bf2f9cf855248475a58df7c42948c61
KarineDHoshi <karinedhoshi@yahoo.com>
2023-08-02 11:23:45 +0200

    remove text from Roadmap container

--- src/pages/AboutDao/AboutDao.tsx
+++ src/pages/AboutDao/AboutDao.tsx
@@ -3,7 +3,7 @@ import './AboutDao.scss';
 
 export default function AboutDao() {
   return (
-    <div className='main'>
+    <div className='main' id="mainDAO">
       <h1>A propos de ChronoDAO</h1>
       <br/>
       <div className="containerDAO">
@@ -31,21 +31,7 @@ export default function AboutDao() {
       <div className='containerMap'>
         <div className="content2">
           <h1>Road Map</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At iusto numquam libero ex quidem minus nesciunt fuga similique, voluptas officiis culpa laboriosam autem nulla perferendis incidunt reprehenderit ipsum eveniet. Excepturi.
-          Quia, enim! Quam itaque odit repellendus cum adipisci est earum, odio qui animi repellat aliquam laboriosam. Voluptatem, fugiat, ipsa aspernatur id earum asperiores quibusdam explicabo, tempora dignissimos ut odio ratione?
-          Ipsa, cum sapiente. Eum impedit quod fugiat culpa facilis dignissimos dolorem nobis fuga nesciunt beatae, vitae non aliquid aliquam corrupti ab temporibus soluta maxime at? Nisi, vero eos. Maxime, delectus.
-          Ex adipisci nemo necessitatibus quasi iusto corrupti illum. Perspiciatis sapiente laborum aliquid, doloremque ex repellendus eius, autem est unde, incidunt illum ab officiis explicabo corrupti non ipsam. Reiciendis, ad optio.
-          Iusto aut nam quam ab eum aliquid! Nesciunt ratione, in ducimus cum nisi unde non facilis molestias, odit, maiores ipsa? Reiciendis mollitia quos beatae eligendi illum iure consequuntur hic molestiae.
-          Ullam unde quod asperiores incidunt esse doloremque inventore recusandae cupiditate autem architecto? Velit ab ducimus debitis animi ex eos quidem at quo rem quibusdam. Similique cum porro numquam saepe minus!
-          Sunt ab accusamus voluptas, facilis dolore delectus harum. Rerum non molestias provident fuga nulla sint illo libero et, veniam voluptatibus, enim quam aliquid debitis minima, harum amet temporibus ratione atque.
-          Laborum aspernatur et architecto nihil quibusdam perspiciatis est officia perferendis enim nesciunt reprehenderit, sunt iste magnam officiis corporis temporibus alias at explicabo repellat deleniti ab dolores nam natus sequi? Placeat?
-          Laboriosam aut totam impedit autem quam illo sit pariatur sequi, a asperiores earum consequatur accusantium corrupti ratione reiciendis ullam obcaecati sint. Delectus, soluta adipisci laborum maiores dolore eos tempore commodi?
-          In ratione quas accusantium voluptatibus nemo dolorum, nostrum reprehenderit error, repellendus dolorem mollitia illo nihil ipsum quis consequuntur vel quod eum deleniti rem sapiente? Nostrum modi exercitationem unde odit ipsam!
-          Distinctio, neque beatae consectetur dolorem veniam in magni et, maiores totam minus sapiente perspiciatis fugit enim sit ut adipisci molestias assumenda! Delectus aliquid quidem eum nostrum ad explicabo. Ipsa, placeat!
-          Unde sapiente qui temporibus iste eaque obcaecati aspernatur accusantium expedita alias molestias pariatur neque quibusdam voluptatum, dicta magni sed rem. Libero vero odio accusantium, placeat obcaecati debitis enim ea tempora?
-          Architecto perferendis debitis est sit voluptatem repudiandae culpa dignissimos earum, quisquam rerum praesentium consequuntur in dolores, nesciunt laborum enim! Tenetur commodi voluptates, id repellendus consequuntur omnis magnam nisi itaque necessitatibus!
-          Quia, illum voluptate velit inventore alias assumenda ipsum repellat mollitia dicta corporis commodi vel nemo voluptas praesentium aspernatur nostrum adipisci earum quasi enim temporibus tempora sapiente. Reiciendis vel illo voluptatem?
-          Voluptatum at suscipit itaque incidunt fuga illum quaerat nisi, voluptate nobis praesentium quidem accusantium commodi sunt ratione, dignissimos architecto cum, quam sed aliquid earum vel explicabo minima sapiente! Obcaecati, odit!</p>
+          <p></p>
         </div>
       </div>
     </div>
