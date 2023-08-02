d9f9b4fe3ec1dd94187ea79c617acd1f52e6af45
KarineDHoshi <karinedhoshi@yahoo.com>
2023-08-02 23:57:48 +0200

    KD change content position

--- src/components/roadmap/Roadmap.tsx
+++ src/components/roadmap/Roadmap.tsx
@@ -2,41 +2,44 @@ import "./Roadmap.scss";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
+
 export default function Roadmap() {
-        useEffect(() => {
-          AOS.init();
-        }, [])
+  useEffect(() => {
+    AOS.init();
+  }, [])
 
   return (
     <main className='main' id="mainRoad">
-      
-    <section>
+      <h3>Roadmap</h3>
 
       <section>
-      <div data-aos="fade-left" data-aos-duration="1500">
-        <h2>hey</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, atque!</p>
-      </div>
+        
+        <div className="section-content" data-aos="fade-left" data-aos-duration="1500">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, atque!</p>
+          <img src="/public/images/roadimage.png" alt="Image 2" className="section-image" />
+
+        </div>
       </section>
 
       <section>
-        <div data-aos="fade-right" data-aos-duration="1500">
-          <h2>hey</h2>
+        <div className="section-content" data-aos="fade-right" data-aos-duration="1500">
+        <img src="/public/images/roadimage.png" alt="Image 2" className="section-image" />
+
+          <h2>Section 2</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, vel.</p>
         </div>
       </section>
 
       <section>
-        <div data-aos="fade-left" data-aos-duration="1500">
-          <h2>hey</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, aut.</p>  
+        <div className="section-content" data-aos="fade-left" data-aos-duration="1500">
+
+          <h2>Section 3</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, aut.</p>
+          <img src="/public/images/roadimage.png" alt="Image 2" className="section-image" />
+
         </div>
-        </section>
-   
       </section>
-   
-    
+
     </main>
   );
-
 }
