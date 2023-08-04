85dc9354e3fbd54f19949647b415a158f4f508ef
KarineDHoshi <karinedhoshi@yahoo.com>
2023-08-04 21:47:10 +0200

    KD update on the animation

--- src/components/roadmap/Roadmap.tsx
+++ src/components/roadmap/Roadmap.tsx
@@ -28,10 +28,10 @@ export default function Roadmap() {
       start: "top 80%",
       onEnter: () => {
         gsap.fromTo(section1, { opacity: 0 }, { opacity: 1, duration: 1 });
-        gsap.fromTo(img1, { rotation: 0 }, { rotation: 180, duration: 5 });
+        gsap.fromTo(img1, { rotation: 0 }, { rotation: 180, duration: 3 });
       },
       onLeaveBack: () => {
-        gsap.fromTo(section1, { opacity: 1 }, { opacity: 0, duration: 1 });
+        gsap.fromTo(section1, { opacity: 1 }, { opacity: 1, duration: 1 });
       },
 
     });
@@ -41,10 +41,10 @@ export default function Roadmap() {
       start: "top 80%",
       onEnter: () => {
         gsap.fromTo(section2, { opacity: 0 }, { opacity: 1, duration: 1 });
-        gsap.fromTo(img2, { rotation: 0 }, { rotation: 180, duration: 5 });
+        gsap.fromTo(img2, { rotation: 0 }, { rotation: 180, duration: 3 });
       },
       onLeaveBack: () => {
-        gsap.fromTo(section2, { opacity: 1 }, { opacity: 0, duration: 1 });
+        gsap.fromTo(section2, { opacity: 1 }, { opacity: 1, duration: 1 });
       },
     });
 
@@ -53,10 +53,10 @@ export default function Roadmap() {
       start: "top 80%",
       onEnter: () => {
         gsap.fromTo(section3, { opacity: 0 }, { opacity: 1, duration: 1 });
-        gsap.fromTo(img3, { rotation: 0 }, { rotation: 180, duration: 5 });
+        gsap.fromTo(img3, { rotation: 0 }, { rotation: 180, duration: 3 });
       },
       onLeaveBack: () => {
-        gsap.fromTo(section3, { opacity: 1 }, { opacity: 0, duration: 1 });
+        gsap.fromTo(section3, { opacity: 1 }, { opacity: 1, duration: 1 });
       },
     });
   }, []);
@@ -95,6 +95,7 @@ export default function Roadmap() {
           
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur enim aliquid saepe sunt cumque perspiciatis, consequatur quidem deleniti velit quaerat aliquam sint animi pariatur quasi? Quisquam, repellat aut. Illo, aperiam?
           Ipsum error ullam unde tenetur maxime? Porro voluptates qui ad unde expedita debitis consequatur maxime dolorum itaque doloribus voluptas distinctio voluptatum, explicabo, beatae eligendi accusantium nesciunt repellendus aut iste tempore.
+
           A placeat consectetur amet incidunt, reiciendis ipsum asperiores repudiandae velit enim beatae dolorum dicta soluta adipisci, esse eveniet. Laboriosam ipsum, eum quasi doloremque tenetur dicta blanditiis voluptas. Odio, eum quibusdam!
           Aut laboriosam ratione, voluptatem eos pariatur ad veritatis et, dolor voluptate qui voluptas. Debitis velit modi itaque quae ut eveniet error dolorem, quibusdam et maiores nam animi odit. Nam, perspiciatis.</p>
           <img src="/public/images/roadimage.png" alt="Image 2" className="section-image" ref={imgRef3}/>
