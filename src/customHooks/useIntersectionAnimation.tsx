56a1958bd1073f8d4d8ee9acacc8868e1acd65f6
Thomas <thomas.kanthack@gmail.com>
2023-08-10 16:13:47 +0200

    fix vercel conflict

--- src/customHooks/useIntersectionAnimation.tsx
+++ src/customHooks/useIntersectionAnimation.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
-export default function useIntersectionAnimation(threshold, animationCallback) {
+export default function useIntersectionAnimation(threshold: number, animationCallback: unknown) {
   const { ref, inView } = useInView({
     threshold: threshold,
   });
-
+  
   useEffect(() => {
+    // eslint-disable-next-line
+    //@ts-ignore
     animationCallback(inView);
   }, [inView, animationCallback]);
 
