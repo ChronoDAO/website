d87394e4b6215025fc78e8090116c5631889b3b6
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-08-18 16:39:46 +0200

    LD creating the useeffect to change the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.scss";
 import Nav from "./components/nav/Nav";
+import NavHamburger from "./components/NavHamburger/NavHamburger";
+import { useEffect, useState } from "react";
 import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home/Home";
 import AboutUs from "./pages/AboutUs/AboutUs";
@@ -8,11 +10,26 @@ import Footer from "./components/footer/Footer.tsx";
 import SocialMedia from "./components/SocialMedia/SocialMedia";
 
 function App() {
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.matchMedia("(min-width: 600px)").matches);
+    };
+
+    handleResize(); // Call it initially
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="container" style={{ 
       backgroundImage: `url("./images/bg_dragon.svg"), url("./images/12.png")` 
     }}>
-      <Nav />
+      {isWideScreen ? <Nav /> : <NavHamburger />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about_us" element={<AboutUs />} />
