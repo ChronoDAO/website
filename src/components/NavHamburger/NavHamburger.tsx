cd06a4f4c1fcb4d46dd918c5c209c115812c852b
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-08-21 16:10:27 +0200

    LD creating the ts responsive hamburger navbar component

--- src/components/NavHamburger/NavHamburger.tsx
+++ src/components/NavHamburger/NavHamburger.tsx
@@ -1,62 +1,58 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import CTAButton from "../Buttons/CTA-Button/CTAButton";
+// import { useState } from "react";
+// import CTAButton from "../Buttons/CTA-Button/CTAButton";
 import "./NavHamburger.scss";
 
 function NavHamburger() {
-  const [isOpen, setIsOpen] = useState(false); // State to track menu open/close
+  // const [showLinks, setShowLinks] = useState(false); // State to track menu open/close
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const links = () => {
-    return (
-      <ul className={`nav__ul ${isOpen ? 'open' : ''}`}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about_dao"> About DAO</Link>
-        </li>
-        <li>
-          <Link to="/about_us">About Us</Link>
-        </li>
-        <li>
-          <Link to="/games">Games</Link>
-        </li>
-      </ul>
-    );
-  };
+  // const handleShowLinks = () => {
+  //   setShowLinks(!showLinks);
+  // };
 
   return (
-    <nav>
-      <div className={`nav__wrapper ${isOpen ? 'open' : ''}`}>
+    // <nav className={`Navbar ${showLinks ? "show-nav": "hide-nav"}`}>
+    <nav className="Navbar">
+      {/* <div className="navbar__logo">
         <a href="/">
           <img src="./images/assets/dao.png" alt="Logo de la guilde ChronoDAO, représente un dragon stylisé bleu dans un cercle rappelant de manière très simplifiée une pocketwatch du jeu Bigtime" />
         </a>
-        <div id="nav-cta-button">
-          <CTAButton text={"Team"} />
-        </div>
-        <div
-          id="menu-toggle"
-          className={isOpen ? 'open' : ''}
-          onClick={toggleMenu}
-        >
-          <div id="hamburger">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <div id="cross">
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        {isOpen && (
+      </div> */}
+      {/* <div id="nav-cta-button">
+        <CTAButton text={"Team"} />
+      </div> */}
+
+      <ul className="navbar__links">
+        <li className="navbar__item">
+          <a href="/" className="navbar__link">Home</a>
+        </li>
+        <li className="navbar__item">
+          <a href="/about_us" className="navbar__link">About us</a>
+        </li>
+        <li className="navbar_item">
+          <a href="/about_dao" className="navbar__link">About DAO</a>
+        </li>
+        <li className="navbar__item">
+          <a href="/games" className="navbar__link">Games</a>
+        </li>
+      </ul>
+      {/* <button className="navbar__burger" onClick={handleShowLinks}> */}
+      <button className="navbar__burger">
+        <span className="burger-bar"></span>
+      </button>
+
+      {/* <div
+        id="hamburger"
+        className={isActive ? 'active' : ''}
+        onClick={toggleMenu}
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      {isActive && (
           <div className="centered-links">{links()}</div>
-        )}
-        </div>
+        )} */}
+
     </nav>
   );
 }
