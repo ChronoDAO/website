b66b2c0ae3b74fb86d997add0d95e5ce1b3fc75f
OlivierMaria <olivier-maria@hotmail.com>
2023-09-18 09:33:52 +0200

    Adding the activeClassName class to the class navbar
    I added the activeClassName class so that when I click on a link it is colored to know which page I am on

--- src/components/common/nav/Nav.tsx
+++ src/components/common/nav/Nav.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import CTAButton from "../Buttons/CTA-Button/CTAButton";
 import "./Nav.scss";
 
@@ -10,15 +10,15 @@ export default function Nav() {
           <img
             src={`
               ${import.meta.env.CDN_URL}/images/assets/icons/dao.png
-              `}
+            `}
             alt="Logo de la guilde ChronoDAO, représente un dragon stylisé bleu dans un cercle rappelant de manière très simplifiée une pocketwatch du jeu Bigtime"
           />
         </a>
         <ul className="nav__ul">
-          <Link to="/">Accueil</Link>
-          <Link to="/about_dao">À propos</Link>
-          <Link to="/about_us">Guilde</Link>
-          <Link to="/games">Jeux</Link>
+          <NavLink exact to="/" activeClassName="active">Accueil</NavLink>
+          <NavLink to="/about_dao" activeClassName="active">À propos</NavLink>
+          <NavLink to="/about_us" activeClassName="active">Guilde</NavLink>
+          <NavLink to="/games" activeClassName="active">Jeux</NavLink>
         </ul>
         <CTAButton text={"Rejoignez nous"} />
       </div>
