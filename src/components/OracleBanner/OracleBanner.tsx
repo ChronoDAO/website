f2a0fb0334529379427bb64a4ad9a8c93b838a5d
OlivierMaria <olivier-maria@hotmail.com>
2023-09-19 16:11:49 +0200

    Oracle banner modification
    I modified the bnnerOracle to ask for an opinion at the text level

--- src/components/OracleBanner/OracleBanner.tsx
+++ src/components/OracleBanner/OracleBanner.tsx
@@ -6,10 +6,10 @@ function OracleBanner() {
   return (
     <div className="wrapp__container">
       <div className="Oracle__wrapper">
-       <h1><a href="/"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'Oracle et dominez!</a></h1>
-       <h1><a href="/"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'Oracle et dominez!</a></h1>
-       <h1><a href="/"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'Oracle et dominez!</a></h1>
-       <h1><a href="/"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'Oracle et dominez!</a></h1>
+       <h2><a href="2"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
+       <h2><a href="/"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
+       <h2><a href="/"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
+       <h2><a href="/"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
        {/* <h1><a href="/"><span className="blue-text">CLIQUEZ</span> pour rejoindre l'Oracle et dominez!</a></h1>
        <h1><a href="/"><span className="blue-text">CLIQUEZ</span> pour rejoindre l'Oracle et dominez!</a></h1>
        <h1><a href="/"><span className="blue-text">CLIQUEZ</span> pour rejoindre l'Oracle et dominez!</a></h1> */}
