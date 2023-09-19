8b6c089288049caea8d17e1ec5d90c14d67f0ad5
OlivierMaria <olivier-maria@hotmail.com>
2023-09-19 16:14:42 +0200

    resolve conflict

--- src/components/OracleBanner/OracleBanner.tsx
+++ src/components/OracleBanner/OracleBanner.tsx
@@ -1,15 +1,14 @@
 import "./OracleBanner.scss"; 
-import logoImg from "../../../public/images/Test/logo.png"
 function OracleBanner() {
 
 
   return (
     <div className="wrapp__container">
       <div className="Oracle__wrapper">
-       <h2><a href="2"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
-       <h2><a href="/"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
-       <h2><a href="/"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
-       <h2><a href="/"><img src={logoImg}/><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
+       <h2><a href="2"><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
+       <h2><a href="/"><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
+       <h2><a href="/"><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
+       <h2><a href="/"><span className="blue-text">CLIQUEZ</span> pour rejoindre l'oracle et éclairer votre destin.</a></h2>
        {/* <h1><a href="/"><span className="blue-text">CLIQUEZ</span> pour rejoindre l'Oracle et dominez!</a></h1>
        <h1><a href="/"><span className="blue-text">CLIQUEZ</span> pour rejoindre l'Oracle et dominez!</a></h1>
        <h1><a href="/"><span className="blue-text">CLIQUEZ</span> pour rejoindre l'Oracle et dominez!</a></h1> */}
