9c33313b62a8fa8957e92ac24105df152232ee4d
Istarengwa <muller.lilian@outlook.fr>
2023-10-03 22:40:57 +0200

    fix title extention and add twitch style

--- src/pages/Twitch/Twitch.tsx
+++ src/pages/Twitch/Twitch.tsx
@@ -1,11 +1,17 @@
+import "./Twitch.scss";
 import CTAButton from "../../components/common/Buttons/CTA-Button/CTAButton"
 
 export default function Twitch() {
   return (
-    <div>
-        <CTAButton text={"CLUF"} link={"https://chronodao.com/extention_twitch/cluf"} />
-        
-        <CTAButton text={"Politique de confidentialité"} link={"https://chronodao.com/extention_twitch/politique_de_confidentialite"} />
-    </div>
+    <>
+        <div id="mainTwitch">
+            <h1>A propos de notre Extention Twitch</h1>
+            <div className="button-container">
+                <CTAButton text={"CLUF"} link={"https://chronodao.com/extention_twitch/cluf"} className={"my-button"}/>
+                
+                <CTAButton text={"Politique de confidentialité"} link={"https://chronodao.com/extention_twitch/politique_de_confidentialite"} className={"my-button"}/>
+            </div>
+        </div>
+    </>
   )
 }
